738b42839b892ff9cbd25e4db643749bdd239309
lindomar <linndomarnascimento17@gmail.com>
2023-02-02 20:50:16 -0300

    aula de constantes

---
+++ constantes.js
@@ -0,0 +1,23 @@
+// ecmascritp
+
+// let = let it change = deixer alterar
+// const = constante = valor fixo 
+
+// const aluno ponteiro = espaco  
+var aluno = {
+  nome: "lindomar",
+  nota1: 2,
+  nota2: 5,
+  nota3: 7,
+  nota4: 6,
+}
+
+const notaFinal = 
+  (aluno.nota1 + aluno.nota2 + aluno.nota3 + aluno.nota4 ) / 4
+
+// console.log
+  // ("o aluno" + aluno.nome + passou: "notaFinal === 5")
+  
+console.log(`o aluno ${aluno.nome} passou: ${notaFinal === 5}`)
+
+console.log(aluno)
